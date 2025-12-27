824e0aefa5c0cd57ed1297f8efe9bea8608627f2
pareronia <49491686+pareronia@users.noreply.github.com>
2025-12-27 06:36:02 +0100

    Problem 92 - faster

--- src/main/java/com/github/pareronia/projecteuler/Problem0092.java
+++ src/main/java/com/github/pareronia/projecteuler/Problem0092.java
@@ -2,10 +2,11 @@ package com.github.pareronia.projecteuler;
 
 import static com.github.pareronia.projecteuler.util.ProblemUtils.lap;
 
+import java.util.stream.IntStream;
+
 public class Problem0092 extends ProblemBase<Long, Long> {
 
-    private Problem0092() {
-    }
+    private Problem0092() {}
 
     public static Problem0092 create() {
         return new Problem0092();
@@ -13,27 +14,26 @@ public class Problem0092 extends ProblemBase<Long, Long> {
 
     @Override
     public Long solve(final Long input) {
-    	long ans = 0;
-    	for (int i = 2; i < input; i++) {
-    		int n = i;
-    		while (true) {
-    			int ss = 0;
-    			while (n > 0) {
-    				final int d = n % 10;
-    				ss += d * d;
-    				n /= 10;
-    			}
-    			if (ss == 89) {
-    				ans++;
-    				break;
-    			}
-    			if (ss == 1) {
-    				break;
-    			}
-    			n = ss;
-    		}
-		}
-    	return ans;
+        return IntStream.range(2, input.intValue()).parallel().filter(this::test).count();
+    }
+
+    private boolean test(final int i) {
+        int n = i;
+        while (true) {
+            int ss = 0;
+            while (n > 0) {
+                final int d = n % 10;
+                ss += d * d;
+                n /= 10;
+            }
+            if (ss == 89) {
+                return true;
+            }
+            if (ss == 1) {
+                return false;
+            }
+            n = ss;
+        }
     }
 
     public static void main(final String[] args) {
